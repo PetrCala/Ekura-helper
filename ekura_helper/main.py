Fixed InGameBot screen position calculation

ekura_helper/main.py
@@ -6,7 +6,7 @@ from pynput.keyboard import Key, HotKey, Controller
 import pytesseract #Text recognition
 import win32.win32gui as win32gui
 
-from scripts.base import Base
+from scripts.base import Base, InGameBot
 from scripts.launcher import Launcher
 from scripts.fisher import Fisher
 from scripts.miner import Miner
@@ -21,14 +21,14 @@ keyboard = Controller()
 
 def main():
     B = Base()
-    M = Miner(char_name = local_settings.CHAR_NAME)
-    # F = Fisher(char_name = 'Bloodbath')
+    # M = Miner(char_name = local_settings.CHAR_NAME)
+    F = Fisher(char_name = 'Stand')
     # L = Launcher()
 
-    # L.inputName()
-    # F.main()
+    # print(F.screen_pos)
+    # F.prepareForFishing()
 
-    M.main() # Mine
+    # M.main() # Mine
 
 if __name__ == '__main__':
     main()